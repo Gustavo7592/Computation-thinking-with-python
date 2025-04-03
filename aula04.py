87ffa16b8690c5fcd092a09d7614df1d010f458d
Gustavo Cavalcanti <gucavalcanti2018@gmail.com>
2025-04-02 21:29:50 -0300

    Exemplos e execicios

---
+++ aula04.py
@@ -0,0 +1,88 @@
+
+# Ex.1 
+# for i in range(10):
+#    print(nome)
+
+# # Ex.2
+
+# for numero in range(1, 11):
+#    print(numero)
+
+# # Ex.3
+
+# for k in range(1, 11, 2):
+#    print(k)
+
+# Ex.4
+
+# for k in range(3):
+#     nome = input("Digite seu nome:")
+#     cpf = int(input("Digite o seu CPF:"))
+#     email = input("Digite seu e-mail:")
+#     print(cpf)
+    
+# Ex.5
+# entrada = input("Digite uma palavra:")
+
+# for i in entrada:
+#     print("Caractere", i)
+
+# Ex.6
+# for i in range(1,11):
+#     if i == 5:
+#         print("Valor encontrado", i)
+#     break
+
+# Ex.7 
+# for k in range(1,6):
+#     if k == 3:
+#         continue
+#     print("Interação", k)
+
+# Ex.8
+# for numero in range(1,5):
+#     num = numero ** 2
+#     print(num)
+
+
+
+# soma_quadrados = 0
+# for k in range(1, 6):
+#     quadrado = k**2
+#     soma_quadrados += quadrado
+#     print(soma_quadrados)
+
+# Ex.9
+# soma_quadrados = 0
+# for k in range(1,5):
+#     quadrado = k**2
+#     soma_quadrados += quadrado
+# media = soma_quadrados / 4
+# print(media)
+
+#### EXERCICIOS #######
+
+# Exercicio - 1 Crie um algoritmo para elevar ao cubo apenas os valores de um range = (2, 12, 2).
+
+# for k in range(2,13,2):
+#     quadrado = k**2
+#     print(quadrado)
+
+# Exercicio - 2 Crie um algoritmo para solicitar 5 valores da compra de produtos em um supermercado. Em seguida, mostre o valor total a ser pago e a media de valores.
+# soma = 0
+# for k in range(1,6):
+#     produtos = int(input("Qual valor do produto?"))
+#     soma += produtos
+# media = soma / 5
+# print(f" o valor total da compra é de {soma} com uma media de {media}")
+
+# Exercicio - 3 Crie um algoritmo que simule uma tabuada. O usuario deverá escolher o valor a qual será multiplicado por 1 ate 10 e mostre os respectivos valores.
+# numero = int(input("Digite um valor"))
+# for tabuada in range(1, 11):
+#     resultado = numero * tabuada
+#     print(resultado)
+    
+# numero = int(input("Digite um numero"))
+# for tabuada in range(1, 10):
+#     resultado = numero * tabuada
+#     print(resultado)
