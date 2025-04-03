ed761bdb4e3e829549beadf69280e448be4ff504
Gustavo Cavalcanti <gucavalcanti2018@gmail.com>
2025-04-02 21:46:16 -0300

    Exemplos e exercicios

---
+++ Aula04/Exemplos04.py
@@ -0,0 +1,71 @@
+
+# Ex.1 
+# for i in range(10):
+#    print(nome)
+
+# # Ex.2
+
+# for numero in range(1, 11):
+#    print(numero)
+
+# # Ex.3
+
+# for k in range(1, 11, 2):
+#    print(k)
+
+# Ex.4
+
+# for k in range(3):
+#     nome = input("Digite seu nome:")
+#     cpf = int(input("Digite o seu CPF:"))
+#     email = input("Digite seu e-mail:")
+#     print(cpf)
+    
+# Ex.5
+# entrada = input("Digite uma palavra:")
+
+# for i in entrada:
+#     print("Caractere", i)
+
+# Ex.6
+# for i in range(1,11):
+#     if i == 5:
+#         print("Valor encontrado", i)
+#     break
+
+# Ex.7 
+# for k in range(1,6):
+#     if k == 3:
+#         continue
+#     print("Interação", k)
+
+# Ex.8
+# for numero in range(1,5):
+#     num = numero ** 2
+#     print(num)
+
+
+
+# soma_quadrados = 0
+# for k in range(1, 6):
+#     quadrado = k**2
+#     soma_quadrados += quadrado
+#     print(soma_quadrados)
+
+# Ex.9
+# soma_quadrados = 0
+# for k in range(1,5):
+#     quadrado = k**2
+#     soma_quadrados += quadrado
+# media = soma_quadrados / 4
+# print(media)
+
+# Ex. 10
+# quantidade = int(input("Digite um numero"))
+# soma = 0
+
+# for k in range (quantidade):
+#     numero = float(input(f"Digite um nimero {k+1}"))
+#     soma += numero
+#     print(soma)
+#     print(numero)
