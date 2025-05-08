5751714b359589ca484ffdbe0fd15e6f3a718d17
Gustavo Cavalcanti <gucavalcanti2018@gmail.com>
2025-05-07 22:05:48 -0300

    Funções

---
+++ Aula08/temp.py
@@ -0,0 +1,13 @@
+# def funcao(parametro1, parametro2):
+#     total = parametro1 * parametro2
+#     return total, ... , ...
+
+
+def eleva_quadrado(x):
+    quadrado = x * x
+    return quadrado
+eleva_quadrado(10)
+
+def segundo_grau(x):
+    resultado = eleva_quadrado(x) + 10 * x + 5
+    return resultado
